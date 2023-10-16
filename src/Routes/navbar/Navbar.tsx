41eadbb671cb6b476762061ad902be38c130b5a4
Kristian Kodehode <kristian007.kodehode@gmail.com>
2023-10-16 14:49:38 +0200

    trying some styling on a Links adn nav

--- src/Routes/navbar/Navbar.tsx
+++ src/Routes/navbar/Navbar.tsx
@@ -3,15 +3,11 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav>
-      <li>
-        <Link to="/">Main</Link>
-      </li>
-      <li>
-        <Link to="/secondary">Secondary</Link>
-      </li>
-      <li>
-        <Link to="/nav3">Nav 3</Link>
-      </li>
+      <Link to="/">Main</Link>
+
+      <Link to="/secondary">Secondary</Link>
+
+      <Link to="/nav3">Nav 3</Link>
     </nav>
   );
 };
