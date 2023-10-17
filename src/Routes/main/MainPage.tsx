f4e166f4099c371b70abdd8611201303d24efb28
Kristian Kodehode <kristian007.kodehode@gmail.com>
2023-10-17 19:06:22 +0200

    added fakestore api to have some content

--- src/Routes/main/MainPage.tsx
+++ src/Routes/main/MainPage.tsx
@@ -1,7 +1,41 @@
+import { useEffect, useState } from "react";
+
+interface Product {
+  id: number;
+  title: string;
+  image: string;
+}
 const Main = () => {
+  const [data, setData] = useState<Product[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://fakestoreapi.com/products");
+        const result = await response.json();
+        setData(result);
+        console.log(result);
+      } catch (error) {
+        console.error("Error fetching data", error);
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <div>
       <h1>This is the main page</h1>
+      <ul>
+        {data.map((product) => (
+          <li key={product.id}>
+            <img
+              src={product.image}
+              alt={product.title}
+              style={{ width: "200px" }}
+            />
+            <h3>{product.title}</h3>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
